3d850e99049e3dbd90f4510248884bae6c2ebd0b
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-22 15:47:29 +0900

    [feat] プロンプトを微調整

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,7 @@
 # Base
-local p_info="%F{105}%n%f %F{102}($(arch))%f"
-local p_cdir="%F{147}%~%f"
+local p_logo="%F{189}%f"
+local p_info="%F{147}%n%f"
+local p_cdir="%F{117}%~%f"
 local p_mark="%B%F{248}❯%f%b"
 local p_time="%F{103}%D %T%f"
 
@@ -14,7 +15,7 @@ precmd () {
     LANG=en_US.UTF-8 vcs_info
     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
-local p_vcs="%1(v|%F{182}%1v%F|)"
+local p_vcs="%1(v|%F{183}%1v%F|)"
 
-PROMPT="$p_info: $p_cdir $p_vcs"$'\n'"$p_mark "
+PROMPT="$p_logo $p_info: $p_cdir $p_vcs"$'\n'"$p_mark "
 RPROMPT="$p_time"
