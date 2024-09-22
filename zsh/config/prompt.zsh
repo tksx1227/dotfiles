b161b3298a13326fe06181b4cbd536cf85ac4549
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-22 13:14:28 +0900

    [feat] プロンプトを更新

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,7 +1,8 @@
 # Base
-local p_cdir="%F{080}%~%f"
-local p_info="%F{082}%n%f %F{009}($(arch))%f"
-local p_mark="%B%F{black}%(!,#,>)%f%b"
+local p_info="%F{105}%n%f %F{102}($(arch))%f"
+local p_cdir="%F{147}%~%f"
+local p_mark="%B%F{248}❯%f%b"
+local p_time="%F{103}%D %T%f"
 
 # Git
 autoload -Uz vcs_info
@@ -13,6 +14,7 @@ precmd () {
     LANG=en_US.UTF-8 vcs_info
     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
-local p_vcs="%1(v|%F{082}%1v%F|)"
+local p_vcs="%1(v|%F{182}%1v%F|)"
 
 PROMPT="$p_info: $p_cdir $p_vcs"$'\n'"$p_mark "
+RPROMPT="$p_time"
