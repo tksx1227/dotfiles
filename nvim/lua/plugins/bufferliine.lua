a563ed0598da54b9d72d4cd09792881d9b97e5ca
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-23 18:53:43 +0900

    [feat] bufferlineを追加

---
+++ nvim/lua/plugins/bufferliine.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        tag = "v4.7.0",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        event = "VeryLazy",
+        keys = {
+            { "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle Pin" },
+            { "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete Non-Pinned Buffers" },
+            { "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Other Buffers" },
+            { "<leader>br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete Buffers to the Right" },
+            { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete Buffers to the Left" },
+            { "<C-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+            { "<C-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+        },
+    }
+}
