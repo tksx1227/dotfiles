534d7acd99e520664d5a2caa23f05bd7630685b9
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-23 16:02:59 +0900

    [feat] nvim-notifyを追加

---
+++ nvim/lua/plugins/nvim-notify.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "rcarriga/nvim-notify",
+        lazy = true,
+        opts = {
+            stages = "static",
+            timeout = 3000,
+            on_open = function(win)
+                vim.api.nvim_win_set_config(win, { zindex = 100 })
+            end,
+        },
+        init = function()
+            vim.notify = require("notify")
+        end
+    },
+}
