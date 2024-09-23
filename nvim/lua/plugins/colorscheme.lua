95058cbf5b14866da07465eaf11ae858631872c9
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-23 12:25:29 +0900

    [feat] 背景を透過する

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
     {
         "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
+        config = function(_, opts)
+            require("tokyonight").setup(opts)
             vim.cmd([[colorscheme tokyonight]])
         end,
     },
