4c8bde571d9f04a17dd7e9148357dd947c8eb17e
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-23 23:38:22 +0900

    [feat] autopairsを追加

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+if false then
+    return {}
+end
+
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    }
+}
