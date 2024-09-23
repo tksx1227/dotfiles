f6cca4012db252b7c3967e1810ddbd80fc19fa2d
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-23 15:38:25 +0900

    [feat] lualineを追加

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        event = "VeryLazy",
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = function()
+            return {
+               options = {
+                   theme = "nightfly",
+               },
+            }
+        end,
+    },
+}
