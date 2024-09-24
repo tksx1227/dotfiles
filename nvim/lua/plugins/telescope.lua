50300cf41e7cb58a92e4e0fad8892fe9d6eb8833
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-24 16:48:52 +0900

    [feat] live_grepを割り当てる

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
             -- find
             { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
             { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" },
-            { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep (cwd)" },
             { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
         },
         opts = function()
@@ -35,6 +35,8 @@ return {
                         n = {
                             ["q"] = actions.close,
                             ["<C-h>"] = "which_key",
+                            ["<C-p>"] = actions.move_selection_previous,
+                            ["<C-n>"] = actions.move_selection_next,
                         },
                     },
                 },
