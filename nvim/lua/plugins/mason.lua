e98294973f8aa5349c5068bd258cae073bf691e3
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-26 11:55:34 +0900

    [feat] masonの設定を追加

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -5,6 +5,28 @@ end
 return {
     {
         "williamboman/mason.nvim",
-        opts = {},
+        dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+        config = function()
+            local mason = require("mason")
+
+            local mason_tool_installer = require("mason-tool-installer")
+
+            -- enable mason and configure icons
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    "stylua", -- lua formatter
+                },
+            })
+        end,
     },
 }
