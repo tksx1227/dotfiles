ea2fac6d1e87219f8d4700ac03b1a92c25dc9647
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-27 18:35:44 +0900

    [feat] cpp追加, python, javascript削除

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -21,9 +21,8 @@ return {
             -- Define your formatters
             formatters_by_ft = {
                 lua = { "stylua" },
-                python = { "isort", "black" },
-                javascript = { "prettierd", "prettier", stop_after_first = true },
                 c = { "clang-format" },
+                cpp = { "clang-format" },
             },
             -- Set default options
             default_format_opts = {
