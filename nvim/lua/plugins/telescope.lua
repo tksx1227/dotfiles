c8c8f2117ea42d0e4681cf2ed9a926c54c42b8bc
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-09-27 17:07:07 +0900

    [feat] 隠しファイルも検索対象にする

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,9 +6,10 @@ return {
         keys = {
             -- find
             { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" },
+            { "<leader>ff", "<cmd>Telescope find_files hidden=true no_ignore=false<cr>", desc = "Find Files (cwd)" },
             { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep (cwd)" },
             { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+            { "<leader>fw", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find Words" },
         },
         opts = function()
             local actions = require("telescope.actions")
@@ -20,6 +21,7 @@ return {
                     selection_caret = " ",
                     preview = {
                         filesize_limit = 0.1, -- MB
+                        hide_on_startup = true,
                     },
                     mappings = {
                         i = {
@@ -37,6 +39,23 @@ return {
                             ["<C-n>"] = actions.move_selection_next,
                             ["<Tab>"] = layout.toggle_preview,
                         },
+                    },
+                    vimgrep_arguments = {
+                        "rg",
+                        "--hidden", -- Search hidden files
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                    },
+                    file_ignore_patterns = {
+                        "^.git/",
+                        "^.cache",
+                        "%.o",
+                        "%.a",
+                        "%.out",
                     },
                 },
             }
