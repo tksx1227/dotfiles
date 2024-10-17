c815c2a4365bdebacf84b481d64760e8894cd09d
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-10-17 10:07:16 +0900

    [feat] fzf用の環境変数を追加

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,3 +3,8 @@ export HISTFILE=~/.zsh_history
 export HISTSIZE=100000
 export SAVEHIST=100000
 export TERM=screen-256color
+
+# fzf settings
+export FZF_CTRL_T_COMMAND="fd --hidden --color never --exclude '.git/'"
+export FZF_CTRL_T_OPTS="--tmux --scheme=path --color=16 --layout=default"
+export FZF_CTRL_R_OPTS="--tmux --scheme=path --color=16 --layout=default"
