8ed95de3a63eabcfee1e6a53145a747ebb168a6f
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2025-08-19 11:10:49 +0900

    [feat] lualineにcopilotのステータスを表示する

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,10 +5,42 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = function()
             return {
-                options = {
-                    theme = "nightfly",
+                sections = {
+                    lualine_x = {
+                        {
+                            "copilot",
+                            symbols = {
+                                status = {
+                                    icons = {
+                                        enabled = " ",
+                                        sleep = " ",
+                                        disabled = " ",
+                                        warning = " ",
+                                        unknown = " ",
+                                    },
+                                    hl = {
+                                        enabled = "#50FA7B",
+                                        sleep = "#AEB7D0",
+                                        disabled = "#6272A4",
+                                        warning = "#FFB86C",
+                                        unknown = "#FF5555",
+                                    },
+                                },
+                                spinners = "dots",
+                                spinner_color = "#6272A4",
+                            },
+                            show_colors = true,
+                            show_loading = true,
+                        },
+                        "encoding",
+                        "fileformat",
+                        "filetype",
+                    },
                 },
             }
         end,
+    },
+    {
+        "AndreM222/copilot-lualine",
     },
 }
