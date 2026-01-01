5745593e12845ef8f14bf1c695d00358626d08bc
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2026-01-01 23:05:45 +0900

    [feat] inclineを編集

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -9,7 +9,7 @@ return {
                 highlight = {
                     groups = {
                         InclineNormal = { guibg = colors.bg_dark1, guifg = colors.green1 },
-                        InclineNormalNC = { guibg = colors.bg_dark1, guifg = terminal_black },
+                        InclineNormalNC = { guibg = colors.bg_dark1, guifg = colors.terminal_black },
                     },
                 },
                 window = {
@@ -23,7 +23,39 @@ return {
                     margin = { horizontal = 0, vertical = 0 },
                     padding = 2,
                 },
-                render = "basic",
+                render = function(props)
+                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+                    local modified = vim.bo[props.buf].modified and "bold,italic" or "bold"
+
+                    local function get_diagnostic_label()
+                        local icons = { error = "󰅚 ", warn = "󰀪 ", hint = "󰌶 ", info = " " }
+                        local label = {}
+
+                        for severity, icon in pairs(icons) do
+                            local n = #vim.diagnostic.get(
+                                props.buf,
+                                { severity = vim.diagnostic.severity[string.upper(severity)] }
+                            )
+                            if n > 0 then
+                                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+                            end
+                        end
+                        if #label > 0 then
+                            table.insert(label, { "┊ " })
+                        end
+                        return label
+                    end
+
+                    local buffer = {
+                        { get_diagnostic_label() },
+                        { filename .. " ", gui = modified },
+                        {
+                            vim.bo[props.buf].modified and " ●" or "",
+                            guifg = props.focused and colors.orange or colors.black,
+                        },
+                    }
+                    return buffer
+                end,
             })
         end,
     },
