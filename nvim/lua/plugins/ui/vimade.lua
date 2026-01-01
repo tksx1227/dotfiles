5f62553cc9d515a7148223c71063fc3677106b21
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2026-01-02 01:35:33 +0900

    [feat] vimade追加

---
+++ nvim/lua/plugins/ui/vimade.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "tadaa/vimade",
+        event = "VeryLazy",
+        opts = {
+            recipe = { "default", { animate = false } },
+            fadelevel = 0.5,
+            ncmode = "windows",
+        },
+    },
+}
