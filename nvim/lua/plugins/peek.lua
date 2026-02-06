6fc08a9edfd1f55ced5599a0b42d0241fa795268
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2026-02-06 13:02:37 +0900

    [feat] peekのhelp用キー追加

--- nvim/lua/plugins/peek.lua
+++ nvim/lua/plugins/peek.lua
@@ -4,11 +4,12 @@ return {
         event = { "VeryLazy" },
         build = "deno task --quiet build:fast", -- NOTE: Denoが必要 (`brew install deno`)
         config = function()
-            require("peek").peek.setup()
+            require("peek").setup()
             vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
             vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
         end,
         keys = {
+            { "<leader>m", desc = "Markdown Preview" },
             { "<leader>mo", "<cmd>PeekOpen<cr>", desc = "Open Markdown Preview" },
             { "<leader>mc", "<cmd>PeekClose<cr>", desc = "Close Markdown Preview" },
         },
