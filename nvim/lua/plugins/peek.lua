0587769b54300743a57b7810a066acf1a3c10f9f
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2026-02-06 12:59:27 +0900

    [feat] markdownプレビュー用プラグインpeekを追加

---
+++ nvim/lua/plugins/peek.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast", -- NOTE: Denoが必要 (`brew install deno`)
+        config = function()
+            require("peek").peek.setup()
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+        keys = {
+            { "<leader>mo", "<cmd>PeekOpen<cr>", desc = "Open Markdown Preview" },
+            { "<leader>mc", "<cmd>PeekClose<cr>", desc = "Close Markdown Preview" },
+        },
+    },
+}
