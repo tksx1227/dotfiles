dc5896adcfe491373831135235c51f18bbb08eaf
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2026-06-16 12:20:08 +0900

    [feat] blameのキーマップを追加

--- nvim/lua/plugins/editor/blame.lua
+++ nvim/lua/plugins/editor/blame.lua
@@ -2,6 +2,9 @@ return {
     {
         "FabijanZulj/blame.nvim",
         lazy = false,
+        keys = {
+            { "<C-g>b", mode = "n", "<cmd>BlameToggle<cr>", desc = "Toggle Blame" },
+        },
         config = function()
             require("blame").setup({})
         end,
